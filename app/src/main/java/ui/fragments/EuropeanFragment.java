70f6eef37a0a590437a5d1caf934b0b057e0cdb8
Abdullah Alali <31869379+aza0092@users.noreply.github.com>
2018-09-26 20:29:23 -0500

    Create EuropeanFragment.java

---
+++ app/src/main/java/ui/fragments/EuropeanFragment.java
@@ -0,0 +1,16 @@
+package ui.fragments;
+
+
+import android.support.v4.app.Fragment;
+
+import R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EuropeanFragment extends CategorizedFragment {
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_european;
+    }
+}
