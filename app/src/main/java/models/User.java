dfd72de04e6d3581ecc74b41cd8e433ed56d3fbd
Abdullah Alali <31869379+aza0092@users.noreply.github.com>
2018-09-26 20:06:42 -0500

    Create User.java

---
+++ app/src/main/java/models/User.java
@@ -0,0 +1,61 @@
+package models;
+
+public class User {
+    private int id;
+    private String username;
+    private String fullname;
+    private String email;
+    private String password;
+
+    public User(String username, String fullname, String email, String password) {
+        this.username = username;
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(int id, String username, String fullname, String email, String password) {
+        this(username, fullname, email, password);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
