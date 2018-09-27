fec2ed1ace7a8371c539d9ea55bad678d3237033
Abdullah Alali <31869379+aza0092@users.noreply.github.com>
2018-09-26 20:18:08 -0500

    Create ToolbarActivity.java

---
+++ app/src/main/java/ui/ToolbarActivity.java
@@ -0,0 +1,8 @@
+package ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public abstract class ToolbarActivity extends AppCompatActivity {
+    protected Toolbar mToolbar;
+}
