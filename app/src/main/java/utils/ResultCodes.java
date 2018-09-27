a5d6f37b48addb2215e598218dd9c0c87309372a
Abdullah Alali <31869379+aza0092@users.noreply.github.com>
2018-09-26 20:37:15 -0500

    Create ResultCodes.java

---
+++ app/src/main/java/utils/ResultCodes.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class ResultCodes {
+    public static final int RECIPE_ADDED = 100;
+    public static final int RECIPE_EDITED = 101;
+    public static final int RECIPE_SHOULD_BE_DELETED = 102;
+    public static final int RECIPE_SHOULD_BE_EDITED = 103;
+}
